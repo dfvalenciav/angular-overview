5e39f51458b4304f72f69738c7d6c56973473d7b
dfvalenciav <dfvalenciav@gmail.com>
2024-01-22 19:55:50 -0500

    creating component

---
+++ src/app/app.modules.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { AboutComponent } from './about/about.component';
+import { TestComponent } from './test/test.component';
+
+@NgModule({
+    declarations: [
+        AboutComponent,
+        TestComponent
+      ],
+    providers: [],
+ })
+ 
+ export class AppModule {}
